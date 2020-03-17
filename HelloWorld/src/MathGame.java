36dcde4e017c106b028df4eec268b33989f714bf
usc-rim003 <rim003@student.usc.edu.au>
2020-03-17 22:33:09 +1000

    Converted Main class to Object Oriented

--- HelloWorld/src/MathGame.java
+++ HelloWorld/src/MathGame.java
@@ -1,32 +1,37 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+//TODO: Week 3 & Week 4
 public class MathGame {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Random random = new Random();
-        int count = 0;
+    ArrayList<Question> questionList = new ArrayList<Question>();
+    Random random = new Random();
+    Scanner input = new Scanner(System.in);
+    int count = 0;
 
+    public MathGame(){
+        this.generateQuestions();
+        this.startQuiz();
+    }
+    private void startQuiz() {
         long timeStart = System.currentTimeMillis();
-        for(int i = 0; i < 10; i++){
-            Question question = new Question(random);
+        for(int i = 0; i < questionList.size(); i++){
+            Question question = questionList.get(i);
 
-            while(true) {
-                question.showQuestion();
+            question.showQuestion();
 
-                int response = input.nextInt();
-                boolean isCorrect = question.checkQuestion( response );
+            int response = input.nextInt();
+            boolean isCorrect = question.checkQuestion( response );
 
-                if( isCorrect ){
-                    count++;
-                    System.out.println("Correct the answer was " + question.answer);
-                    System.out.printf("%s/%s correctly answered %n", count, 10);
-                    break;
-                }else{
-                    System.out.println("Wrong, please try again!");
-                }
+            if( isCorrect ){
+                System.out.println("Correct the answer was " + question.answer);
+                System.out.printf("%s/%s correctly answered the first attempt %n", count, 10);
+            }else{
+                questionList.add(question);
+                System.out.println("Wrong");
             }
+
         }
         long timeEnd = System.currentTimeMillis();
         long timeTaken = (timeEnd - timeStart) / 1000;
@@ -35,5 +40,16 @@ public class MathGame {
         System.out.printf("Time to completion: %s seconds %n", timeTaken);
         System.out.printf("You got %s questions correct out of %s %n", count, 10);
 
+    }
+
+    private void generateQuestions() {
+        for(int i = 0; i < 10; i++){
+            this.questionList.add(new Question(random));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new MathGame();
     }
 }
